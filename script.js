05116fd1d9c413215b7d2e9e2f09d8bd59922ba5
gregorydrule <gregory.drule@hotmail.fr>
2026-04-17 01:10:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,40 @@
 body {
   margin: 0;
   font-family: Arial;
-  background: #f5f5f5;
+  background: linear-gradient(45deg, black, orange);
   display: flex;
-  flex-direction: column;
+  justify-content: center;
   align-items: center;
+  height: 100vh;
+  color: white;
 }
 
 .container {
-  background: white;
-  padding: 20px;
+  background: rgba(0,0,0,0.8);
+  padding: 30px;
   border-radius: 15px;
   text-align: center;
-  margin-top: 20px;
   width: 90%;
   max-width: 400px;
-}
-
-h1 {
-  color: orange;
 }
 
 input {
   padding: 10px;
   width: 80%;
   border-radius: 5px;
-  border: 1px solid #ccc;
+  border: none;
 }
 
 button {
   padding: 10px 20px;
   background: orange;
-  color: white;
   border: none;
+  color: white;
   border-radius: 5px;
+  margin: 5px;
   cursor: pointer;
-}
-
-#message {
-  margin-top: 10px;
-  color: green;
-  font-weight: bold;
 }
 
 ul {
   text-align: left;
-}
-
-#map {
-  height: 300px;
-  width: 90%;
-  margin-top: 20px;
-  border-radius: 10px;
 }
