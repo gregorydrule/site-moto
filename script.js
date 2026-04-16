2b5f173f94b5ceb957d016171f0049b8308d4ebc
gregorydrule <gregory.drule@hotmail.fr>
2026-04-17 00:53:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Site moto prêt !");
+function reserver() {
+  let nom = document.getElementById("nom").value;
+  let date = document.getElementById("date").value;
+
+  if (nom === "" || date === "") {
+    alert("Remplis tous les champs !");
+    return;
+  }
+
+  document.getElementById("message").innerHTML =
+    "✅ Réservation confirmée pour " + nom + " le " + date;
+}
